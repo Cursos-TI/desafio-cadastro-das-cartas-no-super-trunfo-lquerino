1791917aaffc830c9f79d7debef213f9a0c51f58
lquerino <leonardo.querino15@gmail.com>
2025-09-04 13:52:40 +0000

    Nível aventureiro concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,21 +10,32 @@ int main(){
 
     // Definição das variáveis
     
-    char estado1;
-    char nome1[10];
-    int cd1= 1; // A variável codigo1 está fixa em 1 pois trata-se da carta 1.
-    int* populacao1;
-    int* pturistico1;
-    float area1;
-    float pib1;
+    int cd1= 1; // A variável codigo1 está fixa em 1 pois trata-se da carta 1. //
+    int cd2= 2; // A variável codigo1 está fixa em 2 pois trata-se da carta 2. //
 
-    char estado3;
-    char nome2[10];
-    int cd2= 2; // A variável codigo1 está fixa em 2 pois trata-se da carta 2.
-    int* populacao2;
-    int* pturistico2;
-    float area2;
+    char estado1;
+    char estado2;
+
+    char nome1[20];
+    char nome2[20];
+
+    float pib1;
     float pib2;
+
+    float dpp1;
+    float dpp2;
+
+    float rendap1;
+    float rendap2;
+
+    int pp1;
+    int pp2;
+
+    int pturistico1;
+    int pturistico2;
+
+    float area1;
+    float area2;
 
     // Captura de dados das variáveis da Carta 1
 
@@ -34,12 +45,12 @@ int main(){
     scanf("%c", &estado1);
 
     printf("\nDigite o nome desta cidade:");
-    scanf("%s", &nome1);
+    scanf("%s", nome1);
 
     printf("\nDigite o número da população:");
-    scanf("%d", &populacao1);
+    scanf("%d", &pp1);
 
-    printf("\nDigite a área em metros quadrados:");
+    printf("\nDigite a área em km quadrados:");
     scanf("%f", &area1);
 
     printf("\nDigite o PIB em reais (R$):");
@@ -53,15 +64,15 @@ int main(){
     printf("\n\nDigite as informações sobre a Carta 2:\n\n");
 
     printf("Digite o Estado da carta:");
-    scanf("%s", &estado3);
+    scanf("%s", &estado2);
     
     printf("\nDigite o nome desta cidade:");
-    scanf("%s", &nome2);
+    scanf("%s", nome2);
 
     printf("\nDigite o número da população:");
-    scanf("%d", &populacao2);
+    scanf("%d", &pp2);
 
-    printf("\nDigite a área em metros quadrados:");
+    printf("\nDigite a área em km quadrados:");
     scanf("%f", &area2);
 
     printf("\nDigite o PIB em reais (R$):");
@@ -69,6 +80,18 @@ int main(){
 
     printf("\nDigite a quantidade de ponto turístico desta cidade:");
     scanf("%d", &pturistico2);
+
+    // Calculos matemáticos - Nível Aventureiro //
+
+    // Densidade populacional
+
+    dpp1= (float) pp1/area1;
+    dpp2= (float) pp2/area2;
+
+    // Renda per capita
+
+    rendap1= (float) pib1/pp1;
+    rendap2= (float) pib2/pp2;
 
     // Saida de dados da carta 1
 
@@ -78,23 +101,27 @@ int main(){
     printf("Estado: %c\n", estado1);
     printf("Código: %c%d\n", estado1, cd1);
     printf("Nome da cidade: %s\n", nome1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2fm\n", area1);
-    printf("PIB: R$ %.2f\n", pib1);
+    printf("População: %d\n", pp1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f reais\n", pib1);
     printf("Número de pontos turísticos: %d\n", pturistico1);
+    printf("Densidade populacional: %.2f hab/km²\n", dpp1);
+    printf("Renda per capita: %.2f reais", rendap1);
 
      // Saida de dados da carta 2
 
     printf("\n\nInformações sobre a carta:\n\n");
 
     printf("Carta: %d\n", cd2);
-    printf("Estado: %c\n", estado3);
-    printf("Código: %c%d\n", estado3, cd2);
+    printf("Estado: %c\n", estado2);
+    printf("Código: %c%d\n", estado2, cd2);
     printf("Nome da cidade: %s\n", nome2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2fm\n", area2);
-    printf("PIB: R$ %.2f\n", pib2);
+    printf("População: %d\n", pp2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f reais\n", pib2);
     printf("Número de pontos turísticos: %d\n", pturistico2);
+    printf("Densidade populacional: %.2f hab/km²\n", dpp2);
+    printf("Renda per capita: %.2f reais\n", rendap2);
 
     return 0;
 }
